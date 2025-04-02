f067ed4b4fa6016b8a61b78ff76de31256155770
epic-person-on <ryanh41137@gmail.com>
2025-04-02 03:40:54 +0000

    Revert "Add iframe return"
    This reverts commit 4dbdc9bc998ffaa41c8a5df2c83042280ff4f1d7.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index.js');
-var gitpodRouter = require('./routes/gitpod.js');
 
 var app = express();
 
@@ -15,6 +14,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/gitpod', gitpodRouter);
 
 module.exports = app;
